f2da47d60f0482c17b6a61dd3ea85353d2104422
James Prestwich <james@prestwi.ch>
2018-10-15 16:07:13 -0700

    implement whitelist

--- contracts/BringYourOwnWhitelist.sol
+++ contracts/BringYourOwnWhitelist.sol
@@ -4,28 +4,46 @@ contract BringYourOwnWhitelist {
 
     mapping(address => mapping(address => bool)) whitelists;
     mapping(address => bool) whitelistExists;
-    mapping(address => uint) openPositions;  
+    mapping(address => uint) openPositions;
 
-    function noOpenPositions(address user) return (bool);
+    function noOpenPositions(address user) return (bool) {
+        return openPositions[user] == 0;
+    };
 
     function addWhitelistEntries(address[] entries) returns (bool) {
-        // adds entries to the whitelists mapping
         // set whitelistExists = true if false
-    }   
+        if (!whitelistExists[msg.sender]) {
+            whitelistExists[msg.sender] = true;
+        }
+
+        // adds entries to the whitelists mapping
+        for (uint i = 0; i < entries.length; i++) {
+            whitelists[msg.sender][entres[i]] = true;
+        }
+
+        return true;
+    }
 
     function removeWhitelistEntires(address[] entries) returns (bool) {
         // remove entries to the whitelists mapping
-        require(noOpenPositions(msg.sender));    
-    }   
+        require(noOpenPositions(msg.sender));
+
+        // adds entries to the whitelists mapping
+        for (uint i = 0; i < entries.length; i++) {
+            whitelists[msg.sender][entres[i]] = false;
+        }
+
+        return true;
+    }
 
     // only care about checking if someone is NOT on it
     function checkWhitelist(address _entry) return (bool) {
         // check if msg.sender is exists
         if (!whitelistExist[msg.sender]) {
             return true;
-        };  
+        };
         // look for _entry in whitelists
-        // false is _entry is not whitelisted, true if they are 
+        // false is _entry is not whitelisted, true if they are
         return whitelist[msg.sender][_entry];
-    }   
+    }
 }
