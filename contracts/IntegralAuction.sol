22db249784c42f6b39c1b7f56a21951219c5be51
James Prestwich <james@prestwi.ch>
2018-10-15 17:22:50 -0700

    refactor and update

--- contracts/IntegralAuction.sol
+++ contracts/IntegralAuction.sol
@@ -32,24 +32,28 @@ contract IntegralAuction is BringYourOwnWhitelist{
 
     struct Auction {
         AuctionStates state;
-        address seller;                     // Seller address
-        string sellerBtcAddr;               // Seller BTC address
         uint256 ethValue;                   // Eth asset value (wei)
-        bytes input;                        // Seller input
+        address seller;                     // Seller address
+        bytes partialTx;                    // Seller BTC address
         uint256 reservePrice;               // Minimum acceptable bid (sats)
+        uint256 reqDiff;                    // Required number of difficulty in confirmed blocks
+        // Filled Later
         address bidder;                     // Accepted bidder address
         uint256 value;                      // Accepted bid value (sats)
         bytes32 txid;                       // Accepted tx hash
-        uint256 diffSum;                            // Required number of confirmed blocks
     }
 
     address public manager;
     SPVStore public spvStore;  // Deployed contract address of SPVStore.sol
     mapping(bytes32 => Auction) public auctions;
 
-    constructor (address _manager) public { manager = _manager; }
+    constructor (address _manager) public {
+        manager = _manager;
+    }
 
-    function spvStoreAddress(address _spvStoreAddr) public { spvStore = SPVStore(_spvStoreAddr); }
+    function spvStoreAddress(address _spvStoreAddr) public {
+        spvStore = SPVStore(_spvStoreAddr);
+    }
 
     /// @notice
     /// @param _auctionId   Auction identifier
@@ -59,40 +63,32 @@ contract IntegralAuction is BringYourOwnWhitelist{
         return (auctions[_auctionId].seller == _seller);
     }
 
-    /// @notice
-    /// @param _auctionId   Auction identifier
-    /// @param _bidder      Address to check
-    /// @return             true if address is selected Bidder, false otherwise
-    function isBidder(bytes32 _auctionId, address _bidder) public view returns (bool) {
-        return (auctions[_auctionId].bidder == _bidder);
-    }
-
     /// @notice                 Seller opens auction by committing ethereum
     /// @param _partialTx       Seller's partial transaction
     /// @param _seller          Seller's ethereum address
     /// @param _reservePrice    Minimum acceptable bid (sats)
-    /// @param _diffSum         Minimum acceptable block difficulty summation
+    /// @param _reqDiff         Minimum acceptable block difficulty summation
     /// @return                 true if Seller post is valid, false otherwise
     function openAuction(
         address _seller,
         bytes _partialTx,
         uint256 _reservePrice,
-        uint256 _diffSum
+        uint256 _reqDiff
     ) public payable returns (bool) {
 
         // Require Seller to fund tx
-        require(msg.value > 0, "No asset received. Auction must be funded on initialize.");
+        require(msg.value > 0, "No asset received. Auction must be funded on initialization.");
 
         // Auction identifier is sha256 of Seller's parital transaction
         bytes32 _auctionId = _partialTx.hash256();
 
         // Require unique auction identifier
-        require(auctions[_auctionId].state == AuctionStates.NONE);
+        require(auctions[_auctionId].state == AuctionStates.NONE, "Auction exists.");
 
         // Add to auctions mapping
         auctions[_auctionId].seller = _seller;
-        auctions[_auctionId].diffSum = _diffSum;
-        auctions[_auctionId].input = _partialTx;
+        auctions[_auctionId].reqDiff = _reqDiff;
+        auctions[_auctionId].partialTx = _partialTx;
         auctions[_auctionId].ethValue = msg.value;
         auctions[_auctionId].reservePrice = _reservePrice;
         auctions[_auctionId].state = AuctionStates.ACTIVE;
@@ -115,45 +111,34 @@ contract IntegralAuction is BringYourOwnWhitelist{
         // Require auction state to be ACTIVE
         require(auction.state == AuctionStates.ACTIVE);
 
-        // Require summation of submitted block headers difficulty >= diffSum
-        uint256 _diffSum = sumDifficulty(_headers);
-        require(_diffSum >= auction.diffSum);
+        // Require summation of submitted block headers difficulty >= reqDiff
+        require(sumDifficulty(_headers) >= auction.reqDiff);
 
-        bytes memory _header = _headers.slice(0, 80);
-
-        // Submit to SPVStore, get _txid back on success
-        bytes32 _txid = spvStore.validateTransaction(_tx, _proof, _index, _header);
-
-        // Require two inputs
-        require(_tx.extractNumInputs() == 2);
-
-        // Require at least three outputs
+        // Require at least two inputs and at least three outputs
+        require(_tx.extractNumInputs() >= 2);
         require(_tx.extractNumOutputs() >= 3);
 
-        // Require second output to be of OP_RETURN (3) type
+        // Submit to SPVStore, get _txid back on success
+        bytes memory _header = _headers.slice(0, 80);
+        bytes32 _txid = spvStore.validateTransaction(_tx, _proof, _index, _header);
         require(uint(spvStore.getTxOutOutputType(_txid, 1)) == 3);
 
-        uint256 _value = spvStore.getTxOutValue(_txid, 1);
-        bytes memory _payload = spvStore.getTxOutPayload(_txid, 1);
-
-        // Require payload is 20 bytes
-        require(_payload.length == 20);
+        // Update auction state to CLOSED
+        auction.state = AuctionStates.CLOSED;
 
         // Get bidder eth address from OP_RETURN payload bytes
+        bytes memory _payload = spvStore.getTxOutPayload(_txid, 1);
         auction.bidder = _addrFromBytes(_payload);
 
         // Distribute fee and bidder shares
-        _distributeShares(_auctionId);
-
-        // Update auction state to CLOSED
-        auction.state == AuctionStates.CLOSED;
+        _distributeEther(_auctionId);
 
         // Emit AuctionClosed event
         emit AuctionClosed(
             _auctionId,
             auction.bidder,
             auction.seller,
-            _value
+            spvStore.getTxOutValue(_txid, 1)
         );
 
         return true;
@@ -165,14 +150,14 @@ contract IntegralAuction is BringYourOwnWhitelist{
         require(_headers.length % 80 == 0);
 
         // Initialize difficulty summation variable
-        uint256 _diffSum = 0;
+        uint256 _reqDiff = 0;
         uint256 _start = 0;
 
         // For each header, sum its difficulty
         for (uint256 i = 0; i < _headers.length / 80; i++) {
 
             // ith header start index
-            _start = i + i * 80;
+            _start = i * 80;
 
             // ith header
             bytes memory _iHeader = _headers.slice(_start, 80);
@@ -181,12 +166,12 @@ contract IntegralAuction is BringYourOwnWhitelist{
             uint256 _iTarget = _iHeader.extractTarget();
 
             // Add ith header difficulty to difficulty sum
-            _diffSum += _iTarget.calculateDifficulty();
+            _reqDiff += _iTarget.calculateDifficulty();
         }
-        return _diffSum;
+        return _reqDiff;
     }
 
-    function _distributeShares(bytes32 _auctionId) internal returns (bool) {
+    function _distributeEther(bytes32 _auctionId) internal returns (bool) {
         Auction storage auction = auctions[_auctionId];
 
         // Fee share
